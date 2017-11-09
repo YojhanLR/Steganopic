9dd6362ce79d9b376a629be4577c0d0e4a28327d
Yojhan <Yojhanr95@gmail.com>
2017-11-09 13:39:06 -0500

    Base64 imagen en cliente

---
+++ SteganopicMobile/www/js/main.js
@@ -0,0 +1,34 @@
+$("#btn_encode").on("click",function(){
+
+	//console.log('RESULT', readerResult.result)
+	var data = {
+		message: $("#ipt_message").val(),
+		img64: readerResult.result
+	};
+
+	console.log(JSON.stringify(data));
+
+	/*$.ajax({
+		type: 'POST',
+		url: 'http://127.0.0.1:5000/simple_ajax',
+		data: JSON.stringify(data),
+		dataType: 'json',
+        contentType: 'application/json;charset=UTF-8',
+		success: function(result){
+			console.log(result)
+		}
+	});*/
+});
+
+
+var readerResult;
+function encodeImageFileAsURL(element) {
+  var file = element.files[0];
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    console.log('encodeImage')
+    readerResult = reader;
+  }
+  reader.readAsDataURL(file);
+}
+
